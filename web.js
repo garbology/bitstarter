a890b36d59fd9b06ba6419cfe3bfd78c28f5dbb0
Jay <jay@sensationalkids.com.au>
2013-07-08 13:00:31 +0000

    modified web.js to read contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var responseTxt = "";
+var indexFile = 'index.html';
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  try {
+    responseTxt = fs.readFileSync(indexFile).toString();
+  }
+  catch(err) {
+    responseTxt = "Error opening file: " + indexFile;
+  }
+  response.send(responseTxt);
 });
 
 var port = process.env.PORT || 5000;
